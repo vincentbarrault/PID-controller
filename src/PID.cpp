0d348c9b6c839fbabe8e0d8edc2eac651444aa20
Vincent Barrault <vincent.barrault@gmail.com>
2019-01-18 08:49:11 +0100

    Update README with project explanation

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,19 +12,21 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  this->Kp = Kp_;
+  this->Ki = Ki_;
+  this->Kd = Kd_;
 
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+  // Update PID errors based on cte.
+  double old_p_error = p_error;
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - old_p_error;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  // Calculate and return the total error
+  return - (Kp*p_error + Ki*i_error + Kd*d_error);
 }
